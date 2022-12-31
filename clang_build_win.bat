6a7c6faa391e3c1eca46e038f528b4098ef496d5
Jinyao Yan <lingerlyn@gmail.com>
2022-12-31 02:38:33 -0500

    add build for windows

---
+++ clang_build_win.bat
@@ -0,0 +1,14 @@
+nvcc -c src/create_image_cuda.cu -arch=sm_80 -o release2/create_image_cuda.o
+nvcc -c src/ColorSpace.cu -arch=sm_80 -o release2/ColorSpace.o
+@set DIR_IMGUI=lib/imgui
+
+clang++.exe -g -std=c++17 src/main.cpp src/buffer_utils.cpp src/decoder.cpp src/FFmpegDemuxer.cpp src/NvDecoder.cpp %DIR_IMGUI%/imgui.cpp %DIR_IMGUI%/imgui_demo.cpp %DIR_IMGUI%/imgui_draw.cpp %DIR_IMGUI%/imgui_tables.cpp %DIR_IMGUI%/imgui_widgets.cpp %DIR_IMGUI%/backends/imgui_impl_glfw.cpp %DIR_IMGUI%/backends/imgui_impl_opengl3.cpp ^
+-I lib -I lib/nvcodec -I lib/IconFontCppHeaders ^
+-I lib/imgui-filebrowser -I lib/FFmpeg/include -I lib/imgui -I lib/imgui/backends -I "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/include" -I lib/GLFW ^
+release2/create_image_cuda.o release2/ColorSpace.o -o release2/redgui.exe ^
+-L "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/lib/x64" -lcudart -lcuda ^
+-L lib/nvcodec/x64 -lnvencodeapi -lnvcuvid ^
+-L lib/FFmpeg/lib/x64 -lavcodec -lavformat -lavutil -lswresample ^
+-L lib/GL/lib/x64 -lglew32 ^
+-L lib/GLFW/lib-vc2019 -lglfw3 ^
+-lopengl32 -lgdi32 -luser32 -lshell32 -lmsvcrt -lvcruntime -lucrt
