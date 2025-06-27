9dda6f3a2be5902bc6d78d9a417443ff0b09cee6
Jinyao Yan <lingerlyn@gmail.com>
2025-06-26 20:28:34 -0400

    read width and height

--- data_exporter/make_clip.py
+++ data_exporter/make_clip.py
@@ -1,41 +1,47 @@
+import os
 import argparse
 import glob
-import os
 import cv2 as cv
-from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input_folder', type=str, required=True)
-parser.add_argument('-fs', '--frame_start', type=int, required=True)
-parser.add_argument('-fe', '--frame_end', type=int, required=True)
-parser.add_argument('-o', '--output_folder', type=str, required=True)
+parser.add_argument('-i', '--root_dir', type=str, required=True)
+parser.add_argument('-o', '--output_dir', type=str, default="test")
+parser.add_argument('-s', '--frame_start', type=int, required=True)
+parser.add_argument('-e', '--frame_end', type=int, required=True)
 
+# list all cameras, and then load videos, and then save out 
 args = parser.parse_args()
-input_folder = args.input_folder
+root_dir = args.root_dir
+output_dir = args.output_dir
+save_dir = os.path.join(root_dir, output_dir)
 frame_start = args.frame_start
 frame_end = args.frame_end
-number_of_frames = frame_end - frame_start + 1
-output_folder = args.output_folder
 
+os.makedirs(save_dir, exist_ok=True)   
+
+# list all camera videos
 cam_names = []
-for file in glob.glob(input_folder + "/*.mp4"):
+for file in glob.glob(root_dir + "/*.mp4"):
     file_name = file.split("/")
-    cam_names.append(file_name[-1].split(".")[0])
-cam_names.sort()
+    cam_names.append(file_name[-1][:-4])
+cam_names.sort() 
 
-for cam_name in cam_names:
-    video_file = input_folder + "/{}.mp4".format(cam_name)
+for i, cam in enumerate(cam_names):
+    print(i)
+    output_name = os.path.join(save_dir, "{}.mp4".format(cam))
+    video_file = os.path.join(root_dir, "{}.mp4".format(cam))
     video = cv.VideoCapture(video_file)
     video.set(cv.CAP_PROP_POS_FRAMES, frame_start)
-    frame_rate = video.get(cv.CAP_PROP_FPS)
+    width  = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
 
-    fourcc = cv.VideoWriter_fourcc(*'MP4V')
-    output = cv.VideoWriter(
-        "{}/{}.mp4".format(output_folder, cam_name), fourcc, frame_rate, (2200, 3208))
+    fps = video.get(cv.CAP_PROP_FPS)
+    output_dims = (width, height)
+    writer = cv.VideoWriter(output_name, cv.VideoWriter_fourcc('a','v','c','1'), fps, output_dims) 
 
-    for i in tqdm(range(number_of_frames)):
+    for frame_num in range(frame_start, frame_end+1):
         ret, frame = video.read()
-        output.write(frame)
+        writer.write(frame) 
 
-    output.release()
-    video.release()
+    writer.release() 
+    video.release() 
